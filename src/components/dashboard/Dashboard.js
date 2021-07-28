e20606307116c333d40b2b84acfeda0f6b087802
Kevin Ariel Cruz Ortiz <KACruzO@chncentral.chn.com.gt>
2021-07-28 15:56:11 -0600

    agregando logo appbar

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import React, { Fragment } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { AppBar, Toolbar, IconButton, Typography, Button} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import logo from "../../img/chn_logo.jpg";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -27,13 +28,14 @@ const useStyles = makeStyles((theme) => ({
       return (
         <AppBar position="static">
   <Toolbar>
-  <img src="showcase.jpg" alt="logo" className={classes.logo} />
-    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-      <MenuIcon />
-    </IconButton>
+  <img src={logo} className={classes.logo} alt="logo" />
+    
     <Typography variant="h6" className={classes.title}>
       News
     </Typography>
+    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+      <MenuIcon />
+    </IconButton>
     <Button color="inherit">Login</Button>
   </Toolbar>
 </AppBar>
