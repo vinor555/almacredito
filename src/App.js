e132f1eaa41c365e0d5878c6fb0bc486c6f06d50
Kevin Ariel Cruz Ortiz <KACruzO@chncentral.chn.com.gt>
2021-07-28 16:21:47 -0600

    ´cambios´

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Dashboard from './components/dashboard/Dashboard';
+import Formulario from './components/dashboard/Formulario';
 
 
 function App() {
   return (
     <div >
       <Dashboard />
+      <Formulario />
     </div>
   );
 }
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 //Material UI
 import { makeStyles } from '@material-ui/core/styles';
