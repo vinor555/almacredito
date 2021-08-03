1c16e4f8ea0e40130ad39418240738e853ef04b0
Kevin Ariel Cruz Ortiz <KACruzO@chncentral.chn.com.gt>
2021-08-03 14:38:31 -0600

    agregando iconos a pantalla principal

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import './App.css';
 import Dashboard from './components/dashboard/Dashboard';
 import Formulario from './components/dashboard/Formulario';
+import Menu from './components/menu/menu';
 
 
 function App() {
   return (
     <div >
       <Dashboard />
-      <Formulario />
+      <Menu />
     </div>
   );
 }
---
+++ src/components/menu/menu.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import PersonIcon from "@material-ui/icons/Person";
+import DescriptionIcon from "@material-ui/icons/Description";
+import StyleIcon from "@material-ui/icons/Style";
+import GroupAddIcon from '@material-ui/icons/GroupAdd';
+
+const useStyles = makeStyles((theme) => ({
+  externo: {
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+    margin: theme.spacing(20, 0),
+  },
+  main: {
+    margin: theme.spacing(20, 0),
+  },
+  icon: {
+    fontSize: 100,
+  },
+  boton: {
+    flexDirection: "column",
+  },
+}));
+
+export default function Menu() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.main}>
+      <div className={classes.externo}>
+        <label htmlFor="icon-button-file">
+          <IconButton
+            color="primary"
+            aria-label="upload picture"
+            component="span"
+          >
+            <PersonIcon className={classes.icon} />
+            <div>Nuevo</div>
+          </IconButton>
+        </label>
+
+        <label htmlFor="icon-button-file">
+          <IconButton
+            color="primary"
+            aria-label="upload picture"
+            component="span"
+          >
+            <StyleIcon className={classes.icon} />
+            <div>Productos</div>
+          </IconButton>
+        </label>
+        <label htmlFor="icon-button-file">
+          <IconButton
+            color="primary"
+            aria-label="upload picture"
+            component="span"
+          >
+            <DescriptionIcon className={classes.icon} />
+            <div>Perfil Econ. Trans.</div>
+          </IconButton>
+        </label>
+      </div>
+      <div className={classes.externo}>
+        <label htmlFor="icon-button-file">
+          <IconButton
+            color="primary"
+            aria-label="upload picture"
+            component="span"
+          >
+            <GroupAddIcon className={classes.icon} />
+            <div>Referencias</div>
+          </IconButton>
+        </label>
+      </div>
+    </div>
+  );
+}
