0f1cc77c3f6f567f59686336403df4a29ec66b2b
Kevin Ariel Cruz Ortiz <KACruzO@chncentral.chn.com.gt>
2021-08-03 15:05:53 -0600

    agregando rutas

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -4,7 +4,9 @@ import IconButton from "@material-ui/core/IconButton";
 import PersonIcon from "@material-ui/icons/Person";
 import DescriptionIcon from "@material-ui/icons/Description";
 import StyleIcon from "@material-ui/icons/Style";
-import GroupAddIcon from '@material-ui/icons/GroupAdd';
+import GroupAddIcon from "@material-ui/icons/GroupAdd";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Formulario from "../dashboard/Formulario";
 
 const useStyles = makeStyles((theme) => ({
   externo: {
@@ -30,37 +32,44 @@ export default function Menu() {
   return (
     <div className={classes.main}>
       <div className={classes.externo}>
-        <label htmlFor="icon-button-file">
-          <IconButton
-            color="primary"
-            aria-label="upload picture"
-            component="span"
-          >
-            <PersonIcon className={classes.icon} />
-            <div>Nuevo</div>
-          </IconButton>
-        </label>
+        <Router>
+          <Link to="/nuevo">
+            <label htmlFor="icon-button-file">
+              <IconButton
+                color="primary"
+                aria-label="upload picture"
+                component="span"
+              >
+                <PersonIcon className={classes.icon} />
+                <div>Nuevo</div>
+              </IconButton>
+            </label>
+          </Link>
 
-        <label htmlFor="icon-button-file">
-          <IconButton
-            color="primary"
-            aria-label="upload picture"
-            component="span"
-          >
-            <StyleIcon className={classes.icon} />
-            <div>Productos</div>
-          </IconButton>
-        </label>
-        <label htmlFor="icon-button-file">
-          <IconButton
-            color="primary"
-            aria-label="upload picture"
-            component="span"
-          >
-            <DescriptionIcon className={classes.icon} />
-            <div>Perfil Econ. Trans.</div>
-          </IconButton>
-        </label>
+          <label htmlFor="icon-button-file">
+            <IconButton
+              color="primary"
+              aria-label="upload picture"
+              component="span"
+            >
+              <StyleIcon className={classes.icon} />
+              <div>Productos</div>
+            </IconButton>
+          </label>
+          <label htmlFor="icon-button-file">
+            <IconButton
+              color="primary"
+              aria-label="upload picture"
+              component="span"
+            >
+              <DescriptionIcon className={classes.icon} />
+              <div>Perfil Econ. Trans.</div>
+            </IconButton>
+          </label>
+          <Switch>
+            <Route exact path="/nuevo" component={Formulario} />
+          </Switch>
+        </Router>
       </div>
       <div className={classes.externo}>
         <label htmlFor="icon-button-file">
