fa75b25481d26d5f81714191829c977f6b68a37d
Kevin Ariel Cruz Ortiz <KACruzO@chncentral.chn.com.gt>
2021-08-10 16:09:25 -0600

    lista represnetantes avances

--- src/components/dashboard/DatosPersonales.js
+++ src/components/dashboard/DatosPersonales.js
@@ -1,20 +1,14 @@
 import React, { useState, useEffect } from "react";
-import ListaDirecciones from "./ListaDirecciones";
-import ListaCorreos from "./ListaCorreos";
+import IndividualDatosPersonales from "../tiposPersona/IndividualDatosPersonales";
+import JuridicaDatosPersonales from "../tiposPersona/JuridicaDatosPersonales";
 
 //Material UI
-import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 import "date-fns";
-import DateFnsUtils from "@date-io/date-fns";
-import {
-  MuiPickersUtilsProvider,
-  KeyboardDatePicker,
-} from "@material-ui/pickers";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -74,26 +68,16 @@ const useStyles = makeStyles((theme) => ({
 export default function DatosPersonales() {
   const classes = useStyles();
   const [tipoPersona, setTipoPersona] = useState("");
-  const [tipoID, setTipoID] = useState("");
-  const [departamento, setDepartamento] = useState(1);
-  const [municipio, setMunicipio] = useState(5);
-  const [estadoCivil, setEstadoCivil] = useState("");
-  const [sexo, setSexo] = useState("");
-  const [nivelAcademico, setNivelAcademico] = useState("");
 
-  const [selectedDate, setSelectedDate] = useState(
-    new Date("1985-06-15T21:11:54")
-  );
-
-  //consumir el servicio de departamentos
+  //consumir el servicio
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [departamentos, setDepartamentos] = useState([]);
-  const [municipios, setMunicipios] = useState([]);
   const [tiposPersona, setTiposPersona] = useState([]);
 
   useEffect(() => {
-    fetch("https://localhost:9443/middleware/catalogos/tiposPersona/all")
+    fetch(
+      "https://DesaAppVarias11.chncentral.chn.com.gt:9443/middleware/catalogos/tiposPersona/all"
+    )
       .then((res) => res.json())
       .then(
         (data) => {
@@ -109,82 +93,19 @@ export default function DatosPersonales() {
       );
   }, []);
 
-  useEffect(() => {
-    fetch(
-      "https://DesaAppVarias11.chncentral.chn.com.gt:9443/middleware/catalogos/departamentos/all"
-    )
-      .then((res) => res.json())
-      .then(
-        (data) => {
-          console.log(data.departamentos);
-          setIsLoaded(true);
-          setDepartamentos(data.departamentos);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
-  }, []);
-
-  useEffect(() => {
-    fetch(
-      `https://DesaAppVarias11.chncentral.chn.com.gt:9443/middleware/catalogos/municipios/findAllByCodigoDepartamento?codigoDepartamento=${departamento}`
-    )
-      .then((res) => res.json())
-      .then(
-        (data) => {
-          console.log(data.municipios);
-          setIsLoaded(true);
-          setMunicipios(data.municipios);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
-  }, [departamento]);
-
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
-  };
-
   const handleChangeTipoPersona = (event) => {
     setTipoPersona(event.target.value);
   };
 
-  const handleChangeTipoID = (event) => {
-    setTipoID(event.target.value);
-  };
-
-  const handleChangeDepartamento = (event) => {
-    setDepartamento(event.target.value);
-    console.log(departamento);
-  };
-
-  const handleChangeMunicipio = (event) => {
-    setMunicipio(event.target.value);
-  };
-
-  const handleChangeSexo = (event) => {
-    setSexo(event.target.value);
-  };
-
-  const handleChangeEstadoCivil = (event) => {
-    setEstadoCivil(event.target.value);
-  };
-
-  const handleChangeNivelAcademico = (event) => {
-    setNivelAcademico(event.target.value);
-  };
-
-  const DividerWithText = ({ children }) => {
-    return (
-      <div className={classes.container}>
-        <span className={classes.content}>{children}</span>
-      </div>
-    );
-  };
+  function TipoPersona(props){
+    const tipoPersona = props.tipoPersonaProps;
+    if ( tipoPersona === 1 ) {
+        return <IndividualDatosPersonales />;
+    } else if (tipoPersona === 2) {
+      return <JuridicaDatosPersonales />;
+    }
+    return '';
+}
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
@@ -205,259 +126,19 @@ export default function DatosPersonales() {
             onChange={handleChangeTipoPersona}
             label="Tipo de Persona"
           >
-            <MenuItem value="I">Individual</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      </div>
-      <DividerWithText>Identificación</DividerWithText>
-      <div className={classes.section1}>
-        <TextField
-          required
-          id="outlined-required"
-          label="No. ID"
-          defaultValue=""
-          variant="outlined"
-          size="small"
-        />
-        <FormControl
-          variant="outlined"
-          className={classes.formControl}
-          required
-          size="small"
-        >
-          <InputLabel id="demo-simple-select-outlined-label">
-            Tipo de ID
-          </InputLabel>
-          <Select
-            labelId="demo-simple-select-outlined-label"
-            id="demo-simple-select-outlined"
-            value={tipoID}
-            onChange={handleChangeTipoID}
-            label="Tipo de ID"
-          >
-            <MenuItem value="DPI">DPI</MenuItem>
-            <MenuItem value="PASS">Pasaporte</MenuItem>
-          </Select>
-        </FormControl>
-      </div>
-      <DividerWithText>Lugar Emisión Identificación</DividerWithText>
-      <div className={classes.section1}>
-        <FormControl
-          variant="outlined"
-          className={classes.formControl}
-          required
-          size="small"
-        >
-          <InputLabel id="demo-simple-select-outlined-label">
-            Departamento
-          </InputLabel>
-          <Select
-            labelId="demo-simple-select-outlined-label"
-            id="demo-simple-select-outlined"
-            value={departamento}
-            onChange={handleChangeDepartamento}
-            label="Departamento"
-          >
-            {departamentos.map((departamento) => (
+            {tiposPersona.map((tipoPersona) => (
               <MenuItem
-                key={departamento.codigoDepartamento}
-                value={departamento.codigoDepartamento}
+                key={tipoPersona.codigoTipoPersona}
+                value={tipoPersona.codigoTipoPersona}
               >
-                {departamento.descripcion}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl
-          variant="outlined"
-          className={classes.formControl}
-          required
-          size="small"
-        >
-          <InputLabel id="demo-simple-select-outlined-label">
-            Municipio
-          </InputLabel>
-          <Select
-            labelId="demo-simple-select-outlined-label"
-            id="demo-simple-select-outlined"
-            value={municipio}
-            onChange={handleChangeMunicipio}
-            label="Municipio"
-          >
-            {municipios.map((municipio) => (
-              <MenuItem
-                key={municipio.codigoMunicipio}
-                value={municipio.descripcion}
-              >
-                {municipio.descripcion}
+                {tipoPersona.descripcion}
               </MenuItem>
             ))}
           </Select>
         </FormControl>
       </div>
-      <DividerWithText>Nit</DividerWithText>
-      <div className={classes.section1}>
-        <TextField
-          required
-          id="outlined-required"
-          label="Nit"
-          defaultValue=""
-          variant="outlined"
-          size="small"
-        />
-      </div>
-      <DividerWithText>Nombres</DividerWithText>
-      <div className={classes.section1}>
-        <TextField
-          required
-          id="outlined-required"
-          label="Primer Nombre"
-          defaultValue=""
-          variant="outlined"
-          size="small"
-        />
-        <TextField
-          required
-          id="outlined-required"
-          label="Segundo Nombre"
-          defaultValue=""
-          variant="outlined"
-          size="small"
-        />
-        <TextField
-          required
-          id="outlined-required"
-          label="Tercer Nombre"
-          defaultValue=""
-          variant="outlined"
-          size="small"
-        />
-      </div>
-      <DividerWithText>Apellidos</DividerWithText>
-      <div className={classes.section1}>
-        <TextField
-          required
-          id="outlined-required"
-          label="Primer Apellido"
-          defaultValue=""
-          variant="outlined"
-          size="small"
-        />
-        <TextField
-          required
-          id="outlined-required"
-          label="Segundo Apellido"
-          defaultValue=""
-          variant="outlined"
-          size="small"
-        />
-        <TextField
-          required
-          id="outlined-required"
-          label="Apellido Nombre"
-          defaultValue=""
-          variant="outlined"
-          size="small"
-        />
-      </div>
-      <DividerWithText>Datos Generales</DividerWithText>
-      <div className={classes.section1}>
-        <FormControl
-          variant="outlined"
-          className={classes.formControl}
-          required
-          size="small"
-        >
-          <InputLabel id="demo-simple-select-outlined-label">Sexo</InputLabel>
-          <Select
-            labelId="demo-simple-select-outlined-label"
-            id="demo-simple-select-outlined"
-            value={sexo}
-            onChange={handleChangeSexo}
-            label="Sexo"
-          >
-            <MenuItem value="M">Masculino</MenuItem>
-            <MenuItem value="F">Femenino</MenuItem>
-          </Select>
-        </FormControl>
-        <FormControl
-          variant="outlined"
-          className={classes.formControl}
-          required
-          size="small"
-        >
-          <InputLabel id="demo-simple-select-outlined-label">
-            Estado Civil
-          </InputLabel>
-          <Select
-            labelId="demo-simple-select-outlined-label"
-            id="demo-simple-select-outlined"
-            value={estadoCivil}
-            onChange={handleChangeEstadoCivil}
-            label="Estado Civil"
-          >
-            <MenuItem value="S">Soltero/a</MenuItem>
-            <MenuItem value="C">Casado/a</MenuItem>
-          </Select>
-        </FormControl>
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <KeyboardDatePicker
-            variant="outline"
-            margin="normal"
-            id="date-picker-dialog"
-            label="Fecha de Nacimiento"
-            format="dd/MM/yyyy"
-            value={selectedDate}
-            onChange={handleDateChange}
-            KeyboardButtonProps={{
-              "aria-label": "change date",
-            }}
-          />
-        </MuiPickersUtilsProvider>
-      </div>
-      <div className={classes.section1}>
-        <FormControl
-          variant="outlined"
-          className={classes.formControl}
-          required
-          size="small"
-        >
-          <InputLabel id="demo-simple-select-outlined-label">
-            Nivel Académico
-          </InputLabel>
-          <Select
-            labelId="demo-simple-select-outlined-label"
-            id="demo-simple-select-outlined"
-            value={nivelAcademico}
-            onChange={handleChangeNivelAcademico}
-            label="Nivel Académico"
-          >
-            <MenuItem value={1}>Primnaria</MenuItem>
-            <MenuItem value={2}>Básicos</MenuItem>
-            <MenuItem value={3}>Bachillerato</MenuItem>
-            <MenuItem value={4}>Universidad</MenuItem>
-          </Select>
-        </FormControl>
-        <TextField
-          required
-          id="outlined-required"
-          label="Profesión"
-          defaultValue=""
-          variant="outlined"
-          size="small"
-        />
-        <TextField
-          required
-          id="outlined-required"
-          label="Ocupación"
-          defaultValue=""
-          variant="outlined"
-          size="small"
-        />
-      </div>
-      <ListaDirecciones />
-      <ListaCorreos />
+      <TipoPersona tipoPersonaProps={tipoPersona} />
+      
     </form>
   );
 }
---
+++ src/components/tiposPersona/JuridicaDatosPersonales.js
@@ -0,0 +1,283 @@
+import React, { useState } from "react";
+import ListaRepresentantes from "./ListaRepresentantes";
+//Material UI
+
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import "date-fns";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
+import "date-fns";
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    width: "25ch",
+  },
+  formControlLarge: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    width: "52ch",
+  },
+  formControlDir: {
+    width: "100ch",
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+  rootCard: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  section1: {
+    margin: theme.spacing(1, 1),
+  },
+
+  container: {
+    display: "flex",
+    alignItems: "center",
+  },
+  border: {
+    borderBottom: "1px solid lightgray",
+    width: "100%",
+  },
+  content: {
+    paddingTop: theme.spacing(0.5),
+    paddingBottom: theme.spacing(0.5),
+    paddingRight: theme.spacing(2),
+    paddingLeft: theme.spacing(2),
+    fontWeight: 500,
+    fontSize: 20,
+    color: "black",
+  },
+}));
+
+export default function JuridicaDatosPersonales() {
+  const classes = useStyles();
+  const [tipoID, setTipoID] = useState("");
+  const [tipoSociedad, setTipoSociedad] = useState();
+  const [tipoActividad, setTipoActividad] = useState();
+  const [formacion, setFormacion] = React.useState({
+    checkedA: true,
+    checkedB: true,
+  });
+
+  const [selectedDate, setSelectedDate] = useState(
+    new Date("1985-06-15T21:11:54")
+  );
+
+  //consumir el servicio de departamentos
+
+  const handleChangeTipoID = (event) => {
+    setTipoID(event.target.value);
+  };
+
+  const handleChangeTipoSociedad = (event) => {
+    setTipoSociedad(event.target.value);
+  };
+
+  const handleChangeTipoActividad = (event) => {
+    setTipoActividad(event.target.value);
+  };
+
+  const handleChangeFormacion = (event) => {
+    setFormacion({ ...formacion, [event.target.name]: event.target.checked });
+  };
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  const DividerWithText = ({ children }) => {
+    return (
+      <div className={classes.container}>
+        <span className={classes.content}>{children}</span>
+      </div>
+    );
+  };
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      <DividerWithText>Identificación</DividerWithText>
+      <div className={classes.section1}>
+        <TextField
+          required
+          id="outlined-required"
+          label="No. ID"
+          defaultValue=""
+          variant="outlined"
+          size="small"
+        />
+        <FormControl
+          variant="outlined"
+          className={classes.formControl}
+          required
+          size="small"
+        >
+          <InputLabel id="demo-simple-select-outlined-label">
+            Tipo de ID
+          </InputLabel>
+          <Select
+            labelId="demo-simple-select-outlined-label"
+            id="demo-simple-select-outlined"
+            value={tipoID}
+            onChange={handleChangeTipoID}
+            label="Tipo de ID"
+          >
+            <MenuItem value="DPI">DPI</MenuItem>
+            <MenuItem value="PASS">Pasaporte</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <DividerWithText>
+        Datos Generales
+      </DividerWithText>
+      <div className={classes.section1}>
+        <FormControl
+          variant="outlined"
+          className={classes.formControl}
+          required
+          size="small"
+        >
+          <InputLabel id="demo-simple-select-outlined-label">
+            Tipo Sociedad
+          </InputLabel>
+          <Select
+            labelId="demo-simple-select-outlined-label"
+            id="demo-simple-select-outlined"
+            value={tipoSociedad}
+            onChange={handleChangeTipoSociedad}
+            label="Departamento"
+          >
+            <MenuItem key={4} value={4}>
+              Sociedad Anonima
+            </MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <div className={classes.section1}>
+        <FormControlLabel
+          className={classes.formControlSwitch}
+          control={
+            <Switch
+              checked={formacion.checkedB}
+              onChange={handleChangeFormacion}
+              name="checkedB"
+              color="primary"
+            />
+          }
+          label="¿En Formación?"
+        />
+      </div>
+      <div className={classes.section1}>
+        <TextField
+          required
+          id="outlined-required"
+          label="Nombre, razón social o denominación completa"
+          defaultValue=""
+          variant="outlined"
+          size="small"
+          style={{ width: "52ch" }}
+        />
+      </div>
+      <div className={classes.section1}>
+        <TextField
+          required
+          id="outlined-required"
+          label="Nombre Comercial"
+          defaultValue=""
+          variant="outlined"
+          size="small"
+          style={{ width: "52ch" }}
+        />
+      </div>
+      <div className={classes.section1}>
+        <FormControl
+          variant="outlined"
+          className={classes.formControlLarge}
+          required
+          size="small"
+        >
+          <InputLabel id="demo-simple-select-outlined-label">
+            Actividad Económica
+          </InputLabel>
+          <Select
+            labelId="demo-simple-select-outlined-label"
+            id="demo-simple-select-outlined"
+            value={tipoActividad}
+            onChange={handleChangeTipoActividad}
+            label="Actividad Económica"
+          >
+            <MenuItem value={502}>Establecimientos Privados</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <DividerWithText>
+        Datos de la Escritura Pública de Constitución de Sociedad o Entidad
+      </DividerWithText>
+      <div className={classes.section1}>
+        <TextField
+          required
+          id="outlined-required"
+          label="Número"
+          defaultValue=""
+          variant="outlined"
+          size="small"
+        />
+
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <KeyboardDatePicker
+            variant="outline"
+            margin="normal"
+            id="date-picker-dialog"
+            label="Fecha"
+            format="dd/MM/yyyy"
+            value={selectedDate}
+            onChange={handleDateChange}
+            KeyboardButtonProps={{
+              "aria-label": "change date",
+            }}
+          />
+        </MuiPickersUtilsProvider>
+      </div>
+      <div className={classes.section1}>
+        <TextField
+          required
+          id="outlined-required"
+          label="Notario Autorizó"
+          defaultValue=""
+          variant="outlined"
+          size="small"
+          style={{ width: "52ch" }}
+        />
+      </div>
+      <ListaRepresentantes />
+    </form>
+  );
+}
